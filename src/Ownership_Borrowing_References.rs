1cdfa59a5e2e8a87bd30a48d51d2775a0882dc85
Rahul3012 <34572109+Rahul3012@users.noreply.github.com>
2025-03-15 16:37:09 +0530

    Update Ownership_Borrowing_References.rs

--- src/Ownership_Borrowing_References.rs
+++ src/Ownership_Borrowing_References.rs
@@ -9,4 +9,11 @@ fn main(){
 Borrowing:
 fn main() {
     let s1 =String::from("Hello");
+
+    borrow_string(&s1); // borrow s1
+    println!("{}",s1); // this works because we borrowed it, not moved it
+}
+
+fn borrow_string(s: &String){
+    println("{}",s); //we can read the string but not modify it
 }
