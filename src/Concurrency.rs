3aad8c8f2ee413458b9cc03a70d34cf3e6ee7853
Rahul3012 <34572109+Rahul3012@users.noreply.github.com>
2025-03-15 16:55:13 +0530

    Create Concurrency.rs

---
+++ src/Concurrency.rs
@@ -0,0 +1,12 @@
+use std::thread;
+
+fn main(){
+  let handle = thread::spawn(||{
+    println!("Hellow from the spawned thread!");
+  });
+
+  //the main thread waits for the spawned thread to finish
+  handle.join().unwrap();
+
+  println!("Hello from the main thread!");
+}
