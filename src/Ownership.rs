b350b804ab5a8c42f6b96d79fa0d1d1287302bb8
Rahul3012 <34572109+Rahul3012@users.noreply.github.com>
2025-03-15 16:42:18 +0530

    Owner ship borrowing references (#9)
    * temp ownership
    
    * Update Ownership_Borrowing_References.rs
    
    * Update and rename Ownership_Borrowing_References.rs to OwnerShip.rs
    
    * Update Cargo.toml
    
    * Create Borrowing.rs
    
    * Update Borrowing.rs
    
    * Rename OwnerShip.rs to Ownership.rs

---
+++ src/Ownership.rs
@@ -0,0 +1,6 @@
+fn main(){
+    let s1 = String::from("Hello");
+    let s2 = s1; //owbership of hello is moved to s2
+    // println!("{}", s1); This would give an error because s1 is no longer owns the string
+    println!("{}", s2);
+}
