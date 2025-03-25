c462ff3027650db28bc95a7b826269810e701566
Rahul3012 <34572109+Rahul3012@users.noreply.github.com>
2025-03-25 19:55:07 +0530

    Create Error_Handling.rs

---
+++ src/Error_Handling.rs
@@ -0,0 +1,19 @@
+fn divide(a: f64, b: f64) -> Result<f64, String>{
+  if b == 0.0 {
+    Err(String::from("Cannot divide bt zero"))
+  } else {
+    Ok(a/b)
+  }
+}
+
+fn main() {
+  match divide(10.0, 2.0) {
+    Ok(result) => println!("Result: {}", result),
+    Err(e) => println!("Error: {}", e),
+  }
+  
+  match divide(50.0, 0.0) {
+    Ok(result) => println!("Result: {}",result),
+    Err(e) => println!("Error: {}", e),
+  }
+}
