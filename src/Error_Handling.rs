bcae380f8083b63375beb9c0d73dafc300f04484
Rahul3012 <34572109+Rahul3012@users.noreply.github.com>
2025-03-25 19:56:13 +0530

    Update Error_Handling.rs

--- src/Error_Handling.rs
+++ src/Error_Handling.rs
@@ -1,6 +1,6 @@
 fn divide(a: f64, b: f64) -> Result<f64, String>{
   if b == 0.0 {
-    Err(String::from("Cannot divide bt zero"))
+    Err(String::from("Cannot divide by zero"))
   } else {
     Ok(a/b)
   }
