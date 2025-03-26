94fb377e4fa48f99d9a9e1b123966ee08622e337
Rahul3012 <34572109+Rahul3012@users.noreply.github.com>
2025-03-26 21:15:22 +0530

    Create Collections.rs

---
+++ src/Collections.rs
@@ -0,0 +1,15 @@
+fn main() {
+  // Vector
+  let mut numbers = vec![1,2,3,4,5];
+  numbers.push(6);
+  println!("{:?}", numbers);
+
+  // hashmap
+  let mut mao = HashMap::new();
+  map.insert("name","Rahul");
+  map.insert("age","140");
+
+  for(key, value) in &map{
+    println!("Key: {}, value: {}", key,value);
+  }
+}
