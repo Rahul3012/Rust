e8cf19364518baf6718142e2e6f24a42e77f0855
Rahul3012 <34572109+Rahul3012@users.noreply.github.com>
2025-03-26 21:10:46 +0530

    Create Traits.rs (#13)
    * Create Traits.rs
    
    * Update Traits.rs

---
+++ src/Traits.rs
@@ -0,0 +1,26 @@
+trait Speak {
+  fn speak(&self);
+}
+
+struct Dog;
+struct Cat;
+
+impl Speak for Dog {
+  fn speak(&self) {
+    println!("Woof!");
+  }
+}
+
+impl Speak for Cat {
+  fn speak(&self){
+    println!("Meow");
+  }
+}
+
+fn main() {
+  let dog = Dog;
+  let cat = Cat;
+
+  dog.speak();
+  cat.speak();
+}
